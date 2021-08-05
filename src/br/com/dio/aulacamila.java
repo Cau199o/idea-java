52f2b5b558aab7b4e85b313ed4d04da6d295df1b
Cau199o <constantino.acao@gmail.com>
2021-08-05 12:16:47 -0300

    first commit

---
+++ src/br/com/dio/aulacamila.java
@@ -0,0 +1,10 @@
+package br.com.dio;
+
+public class aulacamila {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 3;
+        int c = 5;
+        System.out.println(" Hello World: " + (a+b));
+    }
+}
---
+++ src/br/com/dio/model/gato.java
@@ -0,0 +1,18 @@
+package br.com.dio.model;
+
+public class java {
+    private String nome;
+    private String cor;
+    private Integer idade;
+
+    public java(String nome, String cor, Integer idade) {
+        this.nome = nome;
+        this.cor = cor;
+        this.idade = idade;
+    }
+
+    public java(String nome) {
+
+        this.nome = nome;
+    }
+}
